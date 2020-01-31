b3f0b1d11a4cdc2f4f633a91fa73f106454f0a17
Amen Ayach <aayach@Netways.com>
2020-01-31 13:30:37 +0200

    Initial commit

---
+++ RedisChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RedisChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the connection string:");
+                var connectionString = Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+        }
+    }
+}
